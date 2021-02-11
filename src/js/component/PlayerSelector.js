cc5f2d465bc2f159a4729740b9867d6fc13bab2c
Fabizuni <fabiola.ucr@gmail.com>
2021-02-11 21:26:14 +0000

    updated

--- src/js/component/PlayerSelector.js
+++ src/js/component/PlayerSelector.js
@@ -2,25 +2,58 @@ import React, { Component } from "react";
 import { PropTypes } from "prop-types";
 
 export class PlayerSelector extends Component {
+	constructor(props) {
+		super();
+		this.player1 = null;
+		this.player2 = null;
+	}
+
 	render() {
 		return (
 			<div className="players">
 				<h1>Choose your weapon</h1>
 				<div>
-					<input className="p1" placeholder="player 1"></input>
-					<input className="p2" placeholder="player 2"></input>
+					<input
+						className="p1"
+						placeholder="player 1"
+						onChange={event =>
+							(this.player1 = event.target.value)
+						}></input>
+
+					<input
+						className="p2"
+						placeholder="player 2"
+						onChange={event =>
+							(this.player2 = event.target.value)
+						}></input>
 				</div>
 				<div className="row weapon">
 					<div className="col-3"></div>
-					<div className="col-3">
+					<button
+						className=" col-3 btn btn-info rounded-pill text-center"
+						onClick={() =>
+							this.props.onSetTurn("X", this.player1, this.player)
+						}>
 						<h2>X</h2>
-					</div>
-					<div className="col-3">
+					</button>
+					<button
+						className="col-3 btn btn-info rounded-pill text-center"
+						onClick={() =>
+							this.props.onSetTurn(
+								"O",
+								this.player1,
+								this.player2
+							)
+						}>
 						<h2>O</h2>
-					</div>
+					</button>
 					<div className="col-3"></div>
 				</div>
 			</div>
 		);
 	}
 }
+
+PlayerSelector.propTypes = {
+	onSetTurn: PropTypes.funct
+};
